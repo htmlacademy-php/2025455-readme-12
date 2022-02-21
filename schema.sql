84cc6e3cd0e8e0e49691ac3b4c3f1b81741e0d09
nikitasemenovsm <nikitasemenovsm@gmail.com>
2022-02-21 21:37:24 +0300

    Creating SQL queries

--- schema.sql
+++ schema.sql
@@ -55,7 +55,6 @@ CREATE TABLE subscriptions (
   id INT AUTO_INCREMENT PRIMARY KEY,
   author_user_id INT,
   subscribed_user_id INT
-
 );
 
 CREATE TABLE messages (
@@ -87,12 +86,9 @@ CREATE INDEX class_icon ON content_types(class_icon);
 /*связи*/
 
 /*posts*/
-ALTER TABLE posts
-ADD CONSTRAINT FK_users_posts
-FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE posts
-ADD CONSTRAINT FK_content_types_posts
-FOREIGN KEY (content_types_id) REFERENCES content_types(id);
+ALTER TABLE posts ADD CONSTRAINT FK_users_posts FOREIGN KEY (user_id) REFERENCES users(id);
+
+ALTER TABLE posts ADD CONSTRAINT FK_content_types_posts FOREIGN KEY (content_types_id) REFERENCES content_types(id);
 
 CREATE TABLE posts_hashtags (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -100,49 +96,26 @@ CREATE TABLE posts_hashtags (
   hashtag_id INT
 );
 
-ALTER TABLE posts_hashtags
-ADD CONSTRAINT FK_posts_posts_hashtags
-FOREIGN KEY (post_id) REFERENCES posts(id);
-ALTER TABLE posts_hashtags
-ADD CONSTRAINT FK_hashtags_posts_hashtags
-FOREIGN KEY (hashtag_id) REFERENCES hashtags(id);
+ALTER TABLE posts_hashtags ADD CONSTRAINT FK_posts_posts_hashtags FOREIGN KEY (post_id) REFERENCES posts(id);
+
+ALTER TABLE posts_hashtags ADD CONSTRAINT FK_hashtags_posts_hashtags FOREIGN KEY (hashtag_id) REFERENCES hashtags(id);
+
 /*comments*/
-ALTER TABLE comments
-ADD CONSTRAINT FK_users_comments
-FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE comments
-ADD CONSTRAINT FK_posts_comments
-FOREIGN KEY (post_id) REFERENCES posts(id);
+ALTER TABLE comments ADD CONSTRAINT FK_users_comments FOREIGN KEY (user_id) REFERENCES users(id);
+
+ALTER TABLE comments ADD CONSTRAINT FK_posts_comments FOREIGN KEY (post_id) REFERENCES posts(id);
+
 /*likes*/
-ALTER TABLE likes
-ADD CONSTRAINT FK_users_likes
-FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE likes
-ADD CONSTRAINT FK_posts_likes
-FOREIGN KEY (post_id) REFERENCES posts(id);
+ALTER TABLE likes ADD CONSTRAINT FK_users_likes FOREIGN KEY (user_id) REFERENCES users(id);
+
+ALTER TABLE likes ADD CONSTRAINT FK_posts_likes FOREIGN KEY (post_id) REFERENCES posts(id);
+
 /*subscriptions*/
-ALTER TABLE subscriptions
-ADD CONSTRAINT FK_users_subscriptions_author
-FOREIGN KEY (author_user_id) REFERENCES users(id);
-ALTER TABLE subscriptions
-ADD CONSTRAINT FK_users_subscriptions_subscribed
-FOREIGN KEY (subscribed_user_id) REFERENCES users(id);
+ALTER TABLE subscriptions ADD CONSTRAINT FK_users_subscriptions_author FOREIGN KEY (author_user_id) REFERENCES users(id);
+
+ALTER TABLE subscriptions ADD CONSTRAINT FK_users_subscriptions_subscribed FOREIGN KEY (subscribed_user_id) REFERENCES users(id);
+
 /*messages*/
-ALTER TABLE messages
-ADD CONSTRAINT FK_users_messages_sender
-FOREIGN KEY (sender_id) REFERENCES users(id);
-ALTER TABLE messages
-ADD CONSTRAINT FK_users_messages_recipient
-FOREIGN KEY (recipient_id) REFERENCES users(id);
+ALTER TABLE messages ADD CONSTRAINT FK_users_messages_sender FOREIGN KEY (sender_id) REFERENCES users(id);
 
-
-
-
-
-
-
-
-
-
-
-
+ALTER TABLE messages ADD CONSTRAINT FK_users_messages_recipient FOREIGN KEY (recipient_id) REFERENCES users(id);
